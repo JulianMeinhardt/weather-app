0ad3c0956ab26d289eeaf7e9f2af95eee7edece4
Julian Meinhardt <julianmeinhardt@gmail.com>
2019-01-14 11:22:57 +0100

    Changes on API call and declared Forecast Component
    Change API call to async/await schema and added forecast component to declarations. Also added some sass and html file for forecast component

--- Angular/angular-weather-app/src/app/forecast/forecast.component.ts
+++ Angular/angular-weather-app/src/app/forecast/forecast.component.ts
@@ -1,19 +1,23 @@
 import { Component, OnInit, Injectable } from '@angular/core';
+import APIService from '../services/api.service';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  templateUrl: './forecast.component.html',
+  styleUrls: ['./forecast.component.scss']
 })
 
 @Injectable()
 export class ForecastComponent implements OnInit {
 
-  constructor() {
+  constructor(private apiService: APIService) {
+  }
 
+  async getWeatherInformationByZIP(zip: string) {
+    await this.apiService.getWeatherDataForZip(zip);
   }
 
   ngOnInit() {
-
+    this.getWeatherInformationByZIP('64521');
   }
 }
